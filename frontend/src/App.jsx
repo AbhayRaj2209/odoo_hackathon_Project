6222fb837b6274ca40766000c6057ea129d7abfe
Ishaan Saxena <ishaansaxena145@gmail.com>
2025-07-12 15:36:12 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,45 @@
-// src/App.jsx
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import SkillExchange from './pages/SkillExchange';
-import Profile from './pages/Profile';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import { Route, Routes } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
+import Discover from "./Pages/Discover/Discover";
+import Login from "./Pages/Login/Login";
+import Header from "./Components/Navbar/Navbar";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import AboutUs from "./Pages/AboutUs/AboutUs";
+import Chats from "./Pages/Chats/Chats";
+import Report from "./Pages/Report/Report";
+import Profile from "./Pages/Profile/Profile";
+import NotFound from "./Pages/NotFound/NotFound";
+import Register from "./Pages/Register/Register";
+import Rating from "./Pages/Rating/Rating";
+import EditProfile from "./Pages/EditProfile/EditProfile";
+import PrivateRoutes from "./util/PrivateRoutes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
+const App = () => {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <div className="min-h-screen flex flex-col bg-gray-50">
-          <Navbar />
-          <main className="flex-grow container mx-auto px-4 py-8">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/exchange" element={<SkillExchange />} />
-              <Route path="/profile" element={<Profile />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </BrowserRouter>
-    </AuthProvider>
+    <>
+      <Header />
+      <ToastContainer position="top-right" />
+      <Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route path="/chats" element={<Chats />} />
+        </Route>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/discover" element={<Discover />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about_us" element={<AboutUs />} />
+        <Route path="/edit_profile" element={<EditProfile />} />
+        <Route path="/report/:username" element={<Report />} />
+        <Route path="/profile/:username" element={<Profile />} />
+        <Route path="/rating/:username" element={<Rating />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
