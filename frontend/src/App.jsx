f4fc7337e4377a92c6dd40d20d117a924efff9ea
Ishaan Saxena <ishaansaxena145@gmail.com>
2025-07-12 15:35:48 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,33 @@
 // src/App.jsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import Browse from "./pages/Browse";
-import Requests from "./pages/Requests";
-import "./index.css";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import SkillExchange from './pages/SkillExchange';
+import Profile from './pages/Profile';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <div className="main-wrapper">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/browse" element={<Browse />} />
-            <Route path="/requests" element={<Requests />} />
-          </Routes>
-        </main>
-        <footer className="simple-footer">
-          <p>&copy; 2025 SkillSwap. All rights reserved.</p>
-        </footer>
-      </div>
-    </Router>
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="min-h-screen flex flex-col bg-gray-50">
+          <Navbar />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/exchange" element={<SkillExchange />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   );
-};
+}
 
 export default App;
