3cdc0223d56b2a7cc4ebc0c91de4a22bc7ecc302
Ishaan Saxena <ishaansaxena145@gmail.com>
2025-07-12 23:52:16 +0530

    Update Footer.jsx

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,17 +1,37 @@
 import React from "react";
-import { Container, Navbar } from "react-bootstrap";
+import { Container, Row, Col, Navbar } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <>
-      <Navbar variant="dark" style={{ backgroundColor: "#3BB4A1" }}>
-        <Container className="mx-auto w-100 d-flex justify-content-center">
-          <div className="text-center" style={{ fontFamily: "Montserrat, sans-serif", color: "white" }}>
-            Copyright &copy; 2024 SkillSwap. All rights reserved.
-          </div>
-        </Container>
-      </Navbar>
-    </>
+    <footer style={{ backgroundColor: "#854a68ff", padding: "30px 0" }}>
+      <Container>
+        <Row>
+          {/* Left: Copyright */}
+          <Col
+            md={6}
+            className="text-center text-md-start mb-3 mb-md-0"
+            style={{ fontFamily: "Montserrat, sans-serif", color: "white" }}
+          >
+            <div>© 2025 All rights reserved.</div>
+          </Col>
+
+          {/* Right: Participants */}
+          <Col
+            md={6}
+            className="text-center text-md-end"
+            style={{ fontFamily: "Montserrat, sans-serif", color: "white", fontSize: "0.95rem" }}
+          >
+            <strong>Team Members:</strong>
+            <br />
+            Ishaan Saxena – <a href="mailto:23mc3027@rgipt.ac.in" style={{ color: "#fff" }}>23mc3027@rgipt.ac.in</a>
+            <br />
+            Chinmay Bhardwaj – <a href="mailto:23mc3019@rgipt.ac.in" style={{ color: "#fff" }}>23mc3019@rgipt.ac.in</a>
+            <br />
+            Abhay Raj – <a href="mailto:23it3001@rgipt.ac.in" style={{ color: "#fff" }}>23it3001@rgipt.ac.in</a>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 };
 
